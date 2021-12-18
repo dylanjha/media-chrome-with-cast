250c53d368e371167428b9372476145c4f7074d6
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-12-17 18:17:34 -0800

    try adding chromecast SDK

--- components/player.js
+++ components/player.js
@@ -22,20 +22,69 @@ export default function Player () {
     <div>
       <div className="wrapper">
         <media-controller>
-          <MuxVideoReact ref={muxVideoRef} slot="media" className="video" playbackId="3taBcOqKMfNG029QjBCJMKLviq13OrV6S" />
-          <media-control-bar>
-            <media-play-button></media-play-button>
+          <MuxVideoReact ref={muxVideoRef} slot="media" className="video" playbackId="3taBcOqKMfNG029QjBCJMKLviq13OrV6S" poster="https://image.mux.com/3taBcOqKMfNG029QjBCJMKLviq13OrV6S/thumbnail.jpg?width=500" />
+          <div className="desktop">
+            <media-control-bar>
+              <media-play-button></media-play-button>
+              <media-mute-button></media-mute-button>
+              <media-volume-range></media-volume-range>
+              <media-time-range></media-time-range>
+              {hasAirplay && <AirPlayButton onClick={() => muxVideoRef.current.webkitShowPlaybackTargetPicker()} />}
+              <google-cast-launcher></google-cast-launcher>
+              <media-pip-button></media-pip-button>
+              <media-fullscreen-button></media-fullscreen-button>
+            </media-control-bar>
+          </div>
+          <div className="mobile top-controls" slot="top-chrome">
             <media-mute-button></media-mute-button>
-            <media-volume-range></media-volume-range>
-            <media-time-range></media-time-range>
+            <div className="spacer" />
             {hasAirplay && <AirPlayButton onClick={() => muxVideoRef.current.webkitShowPlaybackTargetPicker()} />}
+            <google-cast-launcher></google-cast-launcher>
             <media-pip-button></media-pip-button>
             <media-fullscreen-button></media-fullscreen-button>
-          </media-control-bar>
+          </div>
+          <div className="mobile centered-controls" slot="centered-chrome">
+            <media-play-button></media-play-button>
+          </div>
         </media-controller>
       </div>
       <style jsx>{`
+        .spacer {
+          flex-grow: 1;
+        }
         .wrapper {
+          max-width: 800px;
+          margin: 0 auto;
+        }
+        .desktop {
+          display: none;
+        }
+        .top-controls {
+          display: flex;
+          width: 100%;
+        }
+        .centered-controls :global(media-play-button) {
+          width: 80px;
+        }
+        .mobile :global(media-control-bar) {
+          width: 100%;
+        }
+        @media(min-width: 768px) {
+          .mobile {
+            display: none;
+            width: 100%;
+          }
+          .desktop {
+            display: block;
+            width: 100%;
+          }
+          .desktop :global(media-control-bar) {
+            width: 100%;
+          }
+        }
+        .wrapper :global(media-controller) {
+          aspect-ratio: 16 / 9;
+          width: 100%;
         }
         .wrapper :global(video) {
           max-width: 100%;
